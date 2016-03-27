35a7bd15202fbe48280b8ca8fe42c2202c1a3375
西伯利亚游牧人 <nomadcube@gmail.com>
2016-03-27 15:21:17 +0800

    feat<recommend>: 由评分矩阵和相似度矩阵计算预测评分矩阵，并针对每一行计算推荐结果

--- recommend.py
+++ recommend.py
@@ -1,8 +1,8 @@
 # coding=utf-8
-from scipy.sparse import csr_matrix
+import numpy as np
 
 
-def item_based_recommend(rating_matrix, similarity_matrix):
+def predict_ratings(rating_matrix, similarity_matrix):
     """
     已知待推荐用户的评分列表，通过物品相似度矩阵计算推荐结果
 
@@ -13,10 +13,25 @@ def item_based_recommend(rating_matrix, similarity_matrix):
     return rating_matrix * similarity_matrix
 
 
+def recommend(predict_mat):
+    """
+    根据预测评分矩阵计算各个用户的k个推荐结果
+
+    :param predict_mat: csr_matrix, 预测评分矩阵
+    :return: array， 各个用户的推荐结果
+    """
+    result = dict()
+    for user_id, each_prediction in enumerate(predict_mat):
+        if each_prediction.nnz > 0:
+            result[user_id] = np.argmax(each_prediction)
+    return result
+
+
 if __name__ == '__main__':
     from read import read
     from similarity_matrix import cosine_similarity
     rating_mat = read("/Users/wumengling/PycharmProjects/movielens_resys/data/small_train_ratings.dat")
     item_simi = cosine_similarity(rating_mat, False)
     test_rating = read("/Users/wumengling/PycharmProjects/movielens_resys/data/small_test_ratings.dat")
-    print item_based_recommend(test_rating, item_simi).shape
+    pred_mat = predict_ratings(test_rating, item_simi)
+    print recommend(pred_mat)
